b9d9aed4a43ad710e5a1b36501784ca8cc0be417
Денис Иванов <go.denis.28@yandex.ru>
2024-05-21 00:02:26 +0500

    Add asyncio

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
+import asyncio
 from fastapi import FastAPI
-from chain import tags_chain, description_chain, image_chain, food_chain
+from chain import (
+    tags_chain, 
+    description_chain, 
+    image_chain, 
+    food_chain
+)
 
 app = FastAPI(title="Product Card Service App")
 
 
 @app.get("/generate/descriptions")
-async def message(title: str):
+async def get_descriptions(title: str):
     descriptions = description_chain.invoke({"title": title})
 
     return {"descriptions": descriptions}
 
 @app.get("/generate/tags")
-async def message(title: str):
+async def get_tags(title: str):
     tags = tags_chain.invoke({"title": title})
 
     return {"tags": tags}
 
 @app.get("/generate/images")
-async def message(title: str):
-    images=[]
+async def get_images(title: str):
+    images = []
     food_list = food_chain.invoke({"title": title})
-    for food in food_list:
-        images.append(image_chain.invoke({"title": food}))
+    tasks = [image_chain.ainvoke({"title": food}) for food in food_list]    
+    image_results = await asyncio.gather(*tasks)
+    images.extend(image_results)
 
     return {"images": images}
+
 
 if __name__ == "__main__":
     import uvicorn
